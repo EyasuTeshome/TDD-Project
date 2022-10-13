9a7ee9c1fec345dd3c20677e12f558c99154571a
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-10-13 19:30:15 +0000

    Add code to make fizzbuzz tet pass

--- solver.rb
+++ solver.rb
@@ -16,4 +16,17 @@ class Solver
       string.reverse
     end
   end
+
+  
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
 end
